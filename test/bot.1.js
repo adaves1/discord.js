7b437987b61c4892bfa984a4a9c5d1227a49cd8e
Anthony Daves <adaves1@yahoo.com>
2025-01-27 15:18:47 +0530

    Create bot.1.js

---
+++ test/bot.1.js
@@ -0,0 +1,29 @@
+var Discord = require("../");
+var client = new Discord.Client();
+var request = require("superagent");
+client.on("debug", (m) => console.log("[debug]", m));
+client.on("warn", (m) => console.log("[warn]", m));
+var start = Date.now();
+
+client.on("message", m => {
+
+	if (m.content === "$$") {
+		client.startTyping(m.channel);
+	} else if (m.content === "!!") {
+		client.stopTyping(m.channel);
+	} else if (m.content === "changename") {
+		client.setUsername("Hydrabot!");
+	} else if (m.content === "setav") {
+		var fs = require("fs");
+		client.setAvatar(fs.readFileSync("./test/image.png"));
+	}
+
+});
+
+function error(e) {
+	console.log(e.stack);
+	process.exit(0);
+}
+
+
+client.login(process.env["discordEmail"], process.env["discordPass"]).catch((e) => console.log(e));
